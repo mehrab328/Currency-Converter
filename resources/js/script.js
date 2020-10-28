c7ba34282f5444fab0317e8808ac967b8002a780
Mehrab <mehrab.zaman@outlook.com>
2020-10-29 00:21:49 +0600

    Design Basic Structure

---
+++ resources/js/script.js
@@ -0,0 +1,4 @@
+const cur1 = document.getElementById("currency1");
+const cur2 = document.getElementById("currency2");
+const inp1 = document.getElementById("inp1");
+const inp2 = document.getElementById("inp2");
